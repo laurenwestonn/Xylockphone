f033b1ffa936e32e30161043d5e3a64868e17694
LaurenWorkLaptop <lauren-weston@hotmail.co.uk>
2017-03-10 23:28:56 +0000

    Skeleton for the CreateUpdate page of the app

---
+++ Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/CreateUpdate.java
@@ -0,0 +1,13 @@
+package com.example.sparelaptop3.xylockphone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_update);
+    }
+}
