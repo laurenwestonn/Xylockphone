058b9559aa4efe183d50aad526eb756aadf90e7a
Gareth Light <gazza.light@virgin.net>
2017-03-11 02:21:11 +0000

    Allowing passwords to be set, retrieved, serialised and deserialised

--- Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/PasswordManager.java
+++ Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/PasswordManager.java
@@ -1,7 +1,13 @@
 package com.example.sparelaptop3.xylockphone;
 
-import java.lang.reflect.Array;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.*;
+
+import android.content.Context;
 import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 /**
@@ -10,26 +16,36 @@ import android.support.v7.app.AppCompatActivity;
 
 public class PasswordManager {
     private PasswordManager(){} //do not instantite
-    private static HashMap passwords;
+    private static HashMap<String, Password> passwords = new HashMap<String, Password>();
     private static AssetManager am;
 
     private static Password getPassword(String appName){
-        return null;
+        return passwords.get(appName);
     }
 
-    static void setPassword(String appName){
-        //something something from the list
+    static void setPassword(String appName, Password password){
+        passwords.put(appName, password);
     }
 
     static void deserialise(AppCompatActivity app){
-        //something something passwords = something;
         am = app.getApplicationContext().getAssets();
+        try {
+            InputStream fi = am.open("passwords.txt");
+            ObjectInputStream is = new ObjectInputStream(fi);
+            passwords = (HashMap<String, Password>) is.readObject();
+        } catch (IOException|ClassNotFoundException i) {
+            i.printStackTrace();
+        }
 
     }
 
     static void serialise(AppCompatActivity app){
-        //something something passwords = something;
-        am = app.getApplicationContext().getAssets();
-
+        try {
+            FileOutputStream fo = app.getApplicationContext().openFileOutput("passwords.txt", Context.MODE_PRIVATE);
+            ObjectOutputStream os = new ObjectOutputStream(fo);
+            os.writeObject(passwords);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
