9b9824255fa7d7e928661d3bc2801bcb06d7589c
DESKTOP-27TNUV9\SPARELAPTOP1 <gradyned@gmail.com>
2017-03-11 20:39:57 +0000

    Added lock screen stuff

--- Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/LockScreen.java
+++ Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/LockScreen.java
@@ -1,26 +1,48 @@
 package com.example.sparelaptop3.xylockphone;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.*;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
-/**
- * Created by SPARELAPTOP1 on 11/03/2017.
- */
+import java.util.EventListener;
 
 public class LockScreen extends AppCompatActivity {
-    public static final int LOCK = 1;
+    private Intent callbackIntent;
+    private Button callbackButton;
+    public static String CALLBACK_INTENT = "CALLBACK_INTENT";
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        onPasswordCorrect();
+        setContentView(R.layout.activity_lock_screen);
+        Bundle extras = getIntent().getExtras();
+        callbackIntent = (Intent) extras.get(CALLBACK_INTENT);
+        Log.d("LockScreen", "onCreate");
+        passwordCorrect();
     }
 
-    private void onPasswordCorrect(){
-        finishActivity(LOCK);
+    public void passwordCorrect(){
+        callbackIntent.putExtra("CALLBACK", "true");
+        Log.d("LockScreen", "passwordCorrect");
+        startActivity(callbackIntent);
     }
 
-    private void onPasswordIncorrect(){
+    private void passwordIncorrect(){
 
     }
+
+    static void goToLockScreen(AppCompatActivity app){
+        Bundle extras = app.getIntent().getExtras();
+        if (extras == null || !extras.getString("CALLBACK").equals("true")) {
+            Intent lockScreenIntent = new Intent(app, LockScreen.class);
+            Intent callbackIntent = new Intent(app, app.getClass());
+            lockScreenIntent.putExtra(LockScreen.CALLBACK_INTENT, callbackIntent);
+            Log.d("LockScreenTest", "Going to lock screen");
+            app.startActivity(lockScreenIntent);
+        }
+        if(extras != null) { extras.clear(); }
+    }
+
+
 }
---
+++ Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/LockScreenTestActivity.java
@@ -0,0 +1,13 @@
+package com.example.sparelaptop3.xylockphone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LockScreenTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lock_screen_test);
+    }
+}
