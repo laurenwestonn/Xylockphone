c6b8b1297fca075e52a916d23ffa86e9ec2305df
DESKTOP-27TNUV9\SPARELAPTOP1 <gradyned@gmail.com>
2017-03-11 00:47:18 +0000

    blah

--- Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/PasswordManager.java
+++ Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/PasswordManager.java
@@ -2,13 +2,16 @@ package com.example.sparelaptop3.xylockphone;
 
 import java.lang.reflect.Array;
 import java.util.*;
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
 /**
  * Created by SPARELAPTOP1 on 10/03/2017.
  */
 
 public class PasswordManager {
     private PasswordManager(){} //do not instantite
-    private HashMap passwords;
+    private static HashMap passwords;
+    private static AssetManager am;
 
     private static Password getPassword(String appName){
         return null;
@@ -18,7 +21,15 @@ public class PasswordManager {
         //something something from the list
     }
 
-    static void deserialise(){
+    static void deserialise(AppCompatActivity app){
         //something something passwords = something;
+        am = app.getApplicationContext().getAssets();
+
+    }
+
+    static void serialise(AppCompatActivity app){
+        //something something passwords = something;
+        am = app.getApplicationContext().getAssets();
+
     }
 }
