98e8edae4951f20757d9e15f75269725bcced27d
LaurenWorkLaptop <lauren-weston@hotmail.co.uk>
2017-03-11 04:50:24 +0000

    Layout of music keys Started on logic to return the note played when a key is pressed. Just don't know how to get the name of an imageButton from a view

--- Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/CreateUpdate.java
+++ Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/CreateUpdate.java
@@ -1,15 +1,39 @@
 package com.example.sparelaptop3.xylockphone;
 
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.logging.Logger;
 
 public class CreateUpdate extends AppCompatActivity {
+
+    public static final String TAG = "YOUR-TAG-NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_update);
+    }
+
+    public void declarePressedNote (View view) {
+
+        //Get the name of the key that was pressed e.g. XyA
+        //Button btnKey = (Button) view;
+        //String buttonName = btnKey.getText().toString();
+
+        //Remove this cheat to make the code work, need to actually get the name of the button clicked
+        String buttonName = "XyA";
+
+        //Convert the name of the key to be the name of the note
+        //e.g. A... get the third character, this is the note name
+        char strNote = buttonName.charAt(2);
+
+        //Print out/log which one was pressed
+        Log.d(TAG, strNote + " was clicked");
+
     }
 }
