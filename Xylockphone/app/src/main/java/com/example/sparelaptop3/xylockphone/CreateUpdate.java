477cc7d3dc8e167e2efa49897166f4b0be530f7a
DESKTOP-OAJTPKC\SPARELAPTOP2 <jakefwilliams1994@gmail.com>
2017-03-11 11:14:15 +0000

    Case StudentHackV: seperated the methods for playing midi notes from CreateUpdate.java into MidiSynthInterface.java

--- Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/CreateUpdate.java
+++ Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/CreateUpdate.java
@@ -9,11 +9,9 @@ import org.billthefarmer.mididriver.MidiDriver;
 import android.view.MotionEvent;
 import android.widget.Button;
 
-public class CreateUpdate extends AppCompatActivity implements MidiDriver.OnMidiStartListener, View.OnTouchListener{
+public class CreateUpdate extends AppCompatActivity implements View.OnTouchListener{
 
-    private MidiDriver midiDriver;
-    private byte[] event;
-    private int[] config;
+    private MidiSynthInterface midiSynth;
     private ImageButton buttonPlayNote;
     public static final String TAG = "YOUR-TAG-NAME";
 
@@ -48,65 +46,28 @@ public class CreateUpdate extends AppCompatActivity implements MidiDriver.OnMidi
         buttonPlayNote = (ImageButton)findViewById(R.id.XyA);
         buttonPlayNote.setOnTouchListener(this);
 
-        // Instantiate the driver.
-        midiDriver = new MidiDriver();
-        // Set the listener.
-        midiDriver.setOnMidiStartListener(this);
+        midiSynth = new MidiSynthInterface();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        midiDriver.start();
+        midiSynth.start();
 
         // Get the configuration.
-        config = midiDriver.config();
+     /*   config = midiDriver.config();
 
         // Print out the details.
         Log.d(this.getClass().getName(), "maxVoices: " + config[0]);
         Log.d(this.getClass().getName(), "numChannels: " + config[1]);
         Log.d(this.getClass().getName(), "sampleRate: " + config[2]);
-        Log.d(this.getClass().getName(), "mixBufferSize: " + config[3]);
+        Log.d(this.getClass().getName(), "mixBufferSize: " + config[3]);*/
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        midiDriver.stop();
-    }
-
-    @Override
-    public void onMidiStart() {
-        Log.d(this.getClass().getName(), "onMidiStart()");
-    }
-
-    private void playNote() {
-
-        // Construct a note ON message for the middle C at maximum velocity on channel 1:
-        event = new byte[3];
-        event[0] = (byte) (0x90 | 0x00);  // 0x90 = note On, 0x00 = channel 1
-        event[1] = (byte) 0x3C;  // 0x3C = middle C
-        event[2] = (byte) 0x7F;  // 0x7F = the maximum velocity (127)
-
-        // Internally this just calls write() and can be considered obsoleted:
-        //midiDriver.queueEvent(event);
-
-        // Send the MIDI event to the synthesizer.
-        midiDriver.write(event);
-
-    }
-
-    private void stopNote() {
-
-        // Construct a note OFF message for the middle C at minimum velocity on channel 1:
-        event = new byte[3];
-        event[0] = (byte) (0x80 | 0x00);  // 0x80 = note Off, 0x00 = channel 1
-        event[1] = (byte) 0x3C;  // 0x3C = middle C
-        event[2] = (byte) 0x00;  // 0x00 = the minimum velocity (0)
-
-        // Send the MIDI event to the synthesizer.
-        midiDriver.write(event);
-
+        midiSynth.stop();
     }
 
     @Override
@@ -117,11 +78,11 @@ public class CreateUpdate extends AppCompatActivity implements MidiDriver.OnMidi
         if (v.getId() == R.id.XyA) {
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 Log.d(this.getClass().getName(), "MotionEvent.ACTION_DOWN");
-                playNote();
+                midiSynth.playNote();
             }
             if (event.getAction() == MotionEvent.ACTION_UP) {
                 Log.d(this.getClass().getName(), "MotionEvent.ACTION_UP");
-                stopNote();
+                midiSynth.stopNote();
             }
         }
 
--- Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/MidiSynthInterface.java
+++ Xylockphone/app/src/main/java/com/example/sparelaptop3/xylockphone/MidiSynthInterface.java
@@ -6,38 +6,57 @@ package com.example.sparelaptop3.xylockphone;
 import android.content.res.Resources;
 
 import org.billthefarmer.mididriver.MidiDriver;
-
-import java.util.Locale;
+import android.util.Log;
 
 public class MidiSynthInterface implements MidiDriver.OnMidiStartListener {
 
-    private int Instrument;
     private MidiDriver midiDriver;
-
+    private byte[] event;
 
     public MidiSynthInterface () {
+        // Instantiate the driver.
         midiDriver = new MidiDriver();
-        if (midiDriver != null)
-            midiDriver.setOnMidiStartListener(this);
-        byte msg[] = new byte[2];
+        // Set the listener.
+        midiDriver.setOnMidiStartListener(this);
     }
 
-    @Override
-    public void onMidiStart()
-    {
+    public void onMidiStart() {
+        Log.d(this.getClass().getName(), "onMidiStart()");
+    }
+
+    public void playNote() {
+
+        // Construct a note ON message for the middle C at maximum velocity on channel 1:
+        event = new byte[3];
+        event[0] = (byte) (0x90 | 0x00);  // 0x90 = note On, 0x00 = channel 1
+        event[1] = (byte) 0x3C;  // 0x3C = middle C
+        event[2] = (byte) 0x7F;  // 0x7F = the maximum velocity (127)
+
+        // Internally this just calls write() and can be considered obsoleted:
+        //midiDriver.queueEvent(event);
+
+        // Send the MIDI event to the synthesizer.
+        midiDriver.write(event);
+    }
+
+    public void stopNote() {
+
+        // Construct a note OFF message for the middle C at minimum velocity on channel 1:
+        event = new byte[3];
+        event[0] = (byte) (0x80 | 0x00);  // 0x80 = note Off, 0x00 = channel 1
+        event[1] = (byte) 0x3C;  // 0x3C = middle C
+        event[2] = (byte) 0x00;  // 0x00 = the minimum velocity (0)
+
+        // Send the MIDI event to the synthesizer.
+        midiDriver.write(event);
 
     }
 
-    public void setInstrument(int instrument) {
-
+    public void stop() {
+        midiDriver.stop();
     }
 
-    public int getInstrument() {
-        return 1;
-    }
-
-
-    public void playNote(int note) {
-
+    public void start() {
+        midiDriver.start();
     }
 }
